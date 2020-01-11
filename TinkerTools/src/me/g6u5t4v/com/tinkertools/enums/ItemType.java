f806c0a65af9c1149c10811f45438643af1dcbd8
6u5t4v <41763143+6u5t4v@users.noreply.github.com>
2020-01-11 15:15:28 +0100

    asd

--- TinkerTools/src/me/g6u5t4v/com/tinkertools/enums/ItemType.java
+++ TinkerTools/src/me/g6u5t4v/com/tinkertools/enums/ItemType.java
@@ -1,9 +1,11 @@
 package me.g6u5t4v.com.tinkertools.enums;
 
+import org.bukkit.inventory.ItemStack;
+
 public enum ItemType {
 
 	HELMET("_HELMET"), CHESTPLATE("_CHESTPLATE"), LEGGINGS("_LEGGINGS"), BOOTS("_BOOTS"), SWORD("_SWORD"), BOW("BOW"),
-	PICKAXE("_PICKAXE"), AXE("_AXE"), SHOVEL("_SHOVEL"), SHEAR("SHEAR"), FISHINGROD("FISHING_ROD");
+	PICKAXE("_PICKAXE"), AXE("_AXE"), SHOVEL("_SHOVEL"), SHEAR("SHEAR"), FISHINGROD("FISHING_ROD"), TRIDENT("TRIDENT");
 
 	private String type;
 
@@ -15,6 +17,15 @@ public enum ItemType {
 		return type;
 	}
 
+	public boolean isEnchantable(ItemStack item) {
+		for(ItemType enchantable : ItemType.values()) {
+			if(item.getType().toString().contains(enchantable.getType())) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
 	public String getItemType(String isType) {
 		for (ItemType str : ItemType.values()) {
 			if (str.getType().equals(isType)) {
